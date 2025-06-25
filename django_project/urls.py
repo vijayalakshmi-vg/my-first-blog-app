0eee0a25bccaee5c63e3baad7e8fb0122680e8be
Viaya L Guntupalli <vijaya.nadendla9@gmail.com>
2025-06-25 17:03:05 -0500

    Add user registration feature

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
- 
+from users import views as user_views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('blog/', include('blog.urls'))
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
