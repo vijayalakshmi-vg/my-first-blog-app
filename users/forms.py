8b99511978061dac853d6baded8114c5ae019c12
Viaya L Guntupalli <lakshmiguntupalli465@gmail.com>
2025-06-29 11:45:07 -0500

    added doc strings to user forms

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
+    """
+    A form for registering new users, extending Django's UserCreationForm.
+    Fields:
+        email (EmailField): Required email address for the user.
+    Meta:
+        model (User): The user model to create.
+        fields (list): List of fields to include in the form: 'username', 'email', 'password1', 'password2'.
+    """
     email = forms.EmailField(required=True)  # Make email required
 
     class Meta:
