4ed1df5e7a02c9fe3faee70d44035df31da53d42
Viaya L Guntupalli <lakshmiguntupalli465@gmail.com>
2025-07-09 18:16:12 -0500

    Add update user profile feature

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 class UserRegisterForm(UserCreationForm):
-    """
-    A form for registering new users, extending Django's UserCreationForm.
-    Fields:
-        email (EmailField): Required email address for the user.
-    Meta:
-        model (User): The user model to create.
-        fields (list): List of fields to include in the form: 'username', 'email', 'password1', 'password2'.
-    """
+    
     email = forms.EmailField(required=True)  # Make email required
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)  # Make email required
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
