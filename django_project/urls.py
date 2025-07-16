b4ff84d8b44dbd40616c69e98a9b87ba2c240f5c
Viaya L Guntupalli <lakshmiguntupalli465@gmail.com>
2025-07-16 10:49:10 -0500

    Add create, update, and delete functionality for blog posts

--- django_project/urls.py
+++ django_project/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('profile/',user_views.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('', include('users.urls')),
     path('', include('django.contrib.auth.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,9 +13,11 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img= Image.open(self.image.path)
+        if self.image and hasattr(self.image, 'path') and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
 
-        if img.height >300 or img.width >300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            # Optional: resize if too big
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
