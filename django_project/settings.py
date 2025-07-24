30565cf9db36da088efc00018cddc8bfdac6ae1b
Viaya L Guntupalli <lakshmiguntupalli465@gmail.com>
2025-07-24 07:23:36 -0500

    Add email and password reset feature

--- django_project/settings.py
+++ django_project/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS= True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD0')
